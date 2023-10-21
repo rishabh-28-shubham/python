0e3d3ffa03bc34ac1889bf2e10dfb5420ee887f9
Rishabh Vishwakarma <ectopdash@gmail.com>
2023-10-21 12:39:22 +0530

    matrix multiplication

---
+++ matrix_multiplication.py
@@ -0,0 +1,25 @@
+def multiply_matrices(mat1, mat2):
+    rows1 = len(mat1)
+    cols1 = len(mat1[0])
+    rows2 = len(mat2)
+    cols2 = len(mat2[0])
+
+    if cols1 != rows2:
+        return "Matrices can't be multiplied"
+
+    result = [[0 for _ in range(cols2)] for _ in range(rows1)]
+
+    for i in range(rows1):
+        for j in range(cols2):
+            for k in range(cols1):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+
+    return result
+
+# Example usage to multiply two matrices
+matrix1 = [[1, 2, 3], [4, 5, 6]]
+matrix2 = [[7, 8], [9, 10], [11, 12]]
+
+result = multiply_matrices(matrix1, matrix2)
+for row in result:
+    print(row)
